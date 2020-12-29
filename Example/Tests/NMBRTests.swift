5a1c4bcfd4d0fec96bbdf301d15019dd994d36aa
Sam Dean <deanWombourne@gmail.com>
2020-12-29 15:59:19 +0000

    Added some tests for precision

--- Example/Tests/NMBRTests.swift
+++ Example/Tests/NMBRTests.swift
@@ -47,3 +47,29 @@ final class NMBRTests: XCTestCase {
         XCTAssertEqual("1.1K", formatter.format(1100))
     }
 }
+
+final class NMBRPrecisionTests: XCTestCase {
+
+    func testSmallNumbers() {
+        let formatter = NMBRFormatter()
+        formatter.precision = 2
+
+        XCTAssertEqual("1", formatter.format(1))
+        XCTAssertEqual("1", formatter.format(1.0))
+        XCTAssertEqual("1.1", formatter.format(1.1))
+        XCTAssertEqual("1.12", formatter.format(1.12))
+        XCTAssertEqual("1.12", formatter.format(1.123))
+        XCTAssertEqual("1.12", formatter.format(1.1234))
+    }
+
+    func testLargeNumbers() {
+        let formatter = NMBRFormatter()
+        formatter.precision = 1
+
+        XCTAssertEqual("1K", formatter.format(1000))
+        XCTAssertEqual("1.1K", formatter.format(1100))
+        XCTAssertEqual("1.1K", formatter.format(1110))
+        XCTAssertEqual("1.1K", formatter.format(1149))
+        XCTAssertEqual("1.2K", formatter.format(1151))
+    }
+}
